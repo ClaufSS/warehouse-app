75a7d4c0049cf8fc9a4b10b123c19a1500435b3a
clauf <claufsts@gmail.com>
2023-04-30 09:31:59 -0300

    Implementando i18n

--- app/models/warehouse.rb
+++ app/models/warehouse.rb
@@ -1,4 +1,4 @@
 class Warehouse < ApplicationRecord
-  validates :name, :code, :city, :address, :cep, :description, presence: true
+  validates :name, :code, :city, :area, :address, :cep, :description, presence: true
   validates :code, uniqueness: true
 end
