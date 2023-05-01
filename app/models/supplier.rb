27b96658ec5b2d1e987951180e00768c1c7569d5
clauf <claufsts@gmail.com>
2023-04-30 23:28:08 -0300

    Revert "Incrementação nos tetes - testando formato do CNPJ"
    This reverts commit ea0c77bc929c04845b9a39d5dfb576b8e6de252d.

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,5 +1,3 @@
 class Supplier < ApplicationRecord
   validates :corporate_name, :brand_name, :registration_number, :full_address, :city, :state, :email, presence: true
-  validates_format_of :registration_number, with: /\A\d{14}\z/, message: 'Deve usar apenas valores numéricos'
-  validates :registration_number, uniqueness: true
 end
